eeb5df3eb905deb685ad3a947432829913bec310
echolocation19 <sakur2420@mail.ru>
2022-02-07 03:46:42 +0300

    added rv with notifyDataSetChanged

--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
@@ -15,8 +16,8 @@ object ShopListRepositoryImpl : ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 10) {
-            addShopItem(ShopItem("name $i", i, true))
+        for (i in 0 until 100) {
+            addShopItem(ShopItem("name $i", i, Random.nextBoolean()))
         }
     }
 
---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,70 @@
+package com.example.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
+import kotlin.RuntimeException
+
+class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ViewHolder>() {
+
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    var onLongClickListener: ((ShopItem) -> Unit)? = null
+    var onClickListener: ((ShopItem) -> Unit)? = null
+
+    companion object {
+        const val VIEW_TYPE_DISABLED = 0
+        const val VIEW_TYPE_ENABLED = 1
+
+        const val MAX_POOL_SIZE = 15
+    }
+
+    /** set val because we need to use view parameter in onBindViewHolder  */
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tvName)
+        val tvCount = view.findViewById<TextView>(R.id.tvCount)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layout = when (viewType) {
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            else -> throw RuntimeException("Unknown view type $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val shopItem = shopList[position]
+        holder.tvName.text = shopItem.name
+        holder.tvCount.text = shopItem.count.toString()
+        holder.view.setOnLongClickListener {
+            onLongClickListener?.invoke(shopItem)
+            true
+        }
+        holder.view.setOnClickListener {
+            onClickListener?.invoke(shopItem)
+        }
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (shopList[position].enabled) VIEW_TYPE_ENABLED else VIEW_TYPE_DISABLED
+    }
+
+    override fun getItemCount(): Int = shopList.size
+}
+
+
+
+
